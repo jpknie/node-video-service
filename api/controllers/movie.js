127f36abb56023aa5887655f44318804506bb5b4
Jani Nieminen <jpkniem@gmail.com>
2014-10-09 09:39:08 +0300

    Implement rest routes for updating, posting and uploading movies (and subtitles)

--- api/controllers/movie.js
+++ api/controllers/movie.js
@@ -1,10 +1,41 @@
+/*
+* This file is part of simple personal video service called MyTube
+* Authored by Jani Nieminen, 2013-2014
+*/
+
+var path = require('path');
+var fs = require('fs-extra');
 var Movie = require('../models/movie');
+var User = require('../models/user');
 
 module.exports.putMovie = function(req, res) {
-	/** User can update his OWN movies */
+	Movie.update({ _id: req.params.movie_id }, { $set: { title: req.body.title, description: req.body.description } }, {upsert: true}, function(err) {
+		if(err) {
+			return res.send(err);
+		}
+		res.json(200, '{ message: "Movie updated" }');
+	});
 };
 
 module.exports.getMovie = function(req, res) {
+	Movie.findOne({ _id: req.params.movie_id }, function(err, movie) {
+		if(err) {
+			return res.json(404, '{ message: "Movie not found!" }');
+		}
+		if(!movie) {
+			return res.json(404, '{ message: "Movie not found!" }');
+		}
+		else {
+			return res.json(movie);
+		}
+	});
+};
+
+module.exports.playMovie = function(req, res) {
+
+};
+
+module.exports.getSubtitle = function(req, res) {
 
 };
 
@@ -24,13 +55,107 @@ module.exports.getMovies = function(req, res) {
 
 };
 
+module.exports.uploadSubtitle = function(req, res) {
+	Movie.findOne({ _id: req.params.movie_id }, function(err, movie) {
+		if(err) {
+			return res.send(err);
+		}
+		if(!movie) {
+			return res.json('{ message: "Cannot attach subtitle file to non-existing data" }');
+		}
+
+		if(movie.userId != req.user.id) {
+			return res.json('{ message: "You are not owning this movie" }');
+		}
+
+		if(movie.hasOwnProperty("subtitle") && movie.subtitle !== undefined) {
+			fs.exists('./media/' + movie.subtitle, function(exists) {
+				if(exists) {
+					fs.unlink('./media/' + movie.subtitle, function(err) {
+						if(err)
+							throw err;
+					});
+				}
+			});
+		}
+
+		if(!req.files)
+			 return	res.json('{ message: "Encountered an error on file upload" }');
+
+		fs.rename(req.files.file.path, './media/' + req.files.file.name, function(err) {
+			if(err)
+				throw err;
+		});
+
+			movie.subtitle = req.files.file.name;
+			movie.save(function(err) {
+				if(err) {
+					return res.json('{ message: "Error while saving data to database" }');
+				}
+			});
+			res.end();
+		});
+}
+
+module.exports.uploadMovie = function(req, res) {
+	Movie.findOne({ _id: req.params.movie_id }, function(err, movie) {
+		if(err) {
+			return res.send(err);
+		}
+
+		if(!movie) {
+			return res.json('{ message: "Cannot attach movie file to non-existing data" }');
+		}
+
+		if(movie.userId != req.user.id) {
+			return res.json('{ message: "You are not owning this movie" }');
+		}
+
+		if(movie.hasOwnProperty("filename") && movie.filename !== undefined) {
+			fs.exists('./media/' + movie.filename, function(exists) {
+				if(exists) {
+					fs.unlink('./media/' + movie.filename, function(err) {
+						if(err)
+							throw err;
+					});
+				}
+			});
+		}
+
+		if(req.files == null)
+			 return	res.json('{ message: "Encountered an error on file upload" }');
+
+		fs.rename(req.files.file.path, './media/' + req.files.file.name, function(err) {
+			if(err)
+				throw err;
+		});
+
+/*
+			var proc = new ffmpeg('./media/'+files.file.name)
+				.takeScreenshots({
+					count: 1,
+					timemarks: ['10']
+			}, './media', function(err) {
+				if(err)
+					throw err;
+			});
+*/
+			movie.filename = req.files.file.name;
+			movie.save(function(err) {
+				if(err) {
+					return res.json('{ message: "Error while saving data to database" }');
+				}
+			});
+			res.end();
+		});
+}
+
 module.exports.postMovies = function(req, res) {
 	var movie = new Movie();
-	console.log("postMovies called");
 	movie.title = req.body.title;
 	movie.description = req.body.description;
 
-//	movie.userId = req.user.userId;
+	movie.userId = req.user.id;
 
 	movie.save(function(err) {
 		if(err) {
--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // Require deps
 var auth = require('./controllers/auth');
+var multipart = require('connect-multiparty');
 var express = require('express');
 var expressJwt = require('express-jwt');
 var jwt = require('jsonwebtoken');
@@ -17,8 +18,8 @@ var db = mongoose.connect("mongodb://localhost/movies");
 
 var app = express();
 
-app.use(bodyParser());
-
+app.use(bodyParser({ defer: true }));
+app.use(multipart({ uploadDir: '/tmp' }));
 /** setup JWT with some secret key */
 var jwtSuperSecretCode = 'super-secret-key';
 var validateJwt = expressJwt({secret: jwtSuperSecretCode});
@@ -35,8 +36,8 @@ app.use('/', function (req, res, next) {
   }
 });
 
-
 // setup cors
+
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -50,13 +51,12 @@ auth.set(app);
 // serve app from server
 app.use(express.static(__dirname + '/client'));
 
+
 var checkUser = function(req, res, next) {
 	if(req.user) {
-		console.log("checkUser user found..");
 		return next();
 	}
 	else {
-		console.log("checkUser user not found..");
 		res.json(403, { message: 'Not authorized' });
 	}
 };
@@ -70,8 +70,14 @@ app.get('/users/me', function(req, res) {
   }
 });
 
-app.get('/movies', checkUser, movieController.getMovies);
-app.post('/movies', checkUser, movieController.postMovies);
+app.get('/api/movies', checkUser, movieController.getMovies);
+app.get('/api/movies/:movie_id', checkUser, movieController.getMovie);
+app.get('/api/movies/:movie_id/play', checkUser, movieController.playMovie);
+app.get('/api/movies/:movie_id/subtitle', checkUser, movieController.getSubtitle);
+app.post('/api/movies/:movie_id/file', checkUser, movieController.uploadMovie);
+app.post('/api/movies/:movie_id/subtitle', checkUser, movieController.uploadSubtitle);
+app.post('/api/movies', checkUser, movieController.postMovies);
+app.put('/api/movies/:movie_id', checkUser, movieController.putMovie);
  
 // listen on port
 var server = app.listen(3000, function() {
